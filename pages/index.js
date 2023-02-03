5b20d6cccdd1b6083f855746d8307df86c904a83
JUNYA-727 <k.junya0727@icloud.com>
2023-02-03 18:38:39 +0900

    image path

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,12 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1>hello</h1>
-      <Image src={"/_A6A5059.jpg"} width={100} height={100} alt=""></Image>
+      <Image
+        src={"https://junya-727.github.io/blog/_A6A5059.jpg"}
+        width={100}
+        height={100}
+        alt=""
+      ></Image>
     </div>
   );
 }
