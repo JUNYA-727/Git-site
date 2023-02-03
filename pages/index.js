8b76072b09ab3f3401e1ce07611928741360b71c
JUNYA-727 <k.junya0727@icloud.com>
2023-02-03 21:04:57 +0900

    master

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
           width: "100%",
         }}
       >
-        <h2 className="fadeup-name">JUNYA KURAMOCHI</h2>
+        <h3 className="fadeup-name">JUNYA KURAMOCHI</h3>
         <div className="fadeup-logo">
           <IconButton
             variant="outlined"
@@ -57,7 +57,10 @@ export default function Home() {
             ></TwitterIcon>
           </IconButton>
 
-          <IconButton variant="outlined" href="/notfound">
+          <IconButton
+            variant="outlined"
+            href="https://junya-727.github.io/blog/notfound"
+          >
             <WebAssetIcon
               fontSize="large"
               style={{ color: "#33FF00" }}
