65a9a18a682ba31e00885b2ec1b0c43c6d8a4230
JUNYA-727 <k.junya0727@icloud.com>
2023-02-09 14:14:24 +0900

    mail change

--- pages/career.js
+++ pages/career.js
@@ -3,12 +3,42 @@
 import Image from "next/image";
 
 import { Link as Scroll } from "react-scroll";
-
+import { useState } from "react";
 import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Scrollbar, A11y, Autoplay } from "swiper";
 import "swiper/css";
+import { useInView } from "react-intersection-observer";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import { IconButton } from "@mui/material";
+
 export default function Notfound() {
+  const [send, setSend] = useState(true);
+
+  const registerUser = async (event) => {
+    event.preventDefault();
+
+    const res = await fetch("/api/send", {
+      body: JSON.stringify({
+        name: event.target.name.value,
+        email: event.target.email.value,
+        message: event.target.message.value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+
+    const result = await res.json();
+  };
+  const [ref1, inview1] = useInView({
+    rootMargin: "-1px",
+  });
+
+  //https://junya-727.github.io/blog
   return (
     <div
       id="main"
@@ -17,7 +47,6 @@ export default function Notfound() {
         alignItems: "center",
         backgroundColor: "f5f3f5",
         paddingTop: "0px",
-        paddingBottom: "20%",
       }}
       className="fadeup-overall"
     >
@@ -31,11 +60,23 @@ export default function Notfound() {
           zIndex: "200",
         }}
       >
-        <ArrowCircleUpIcon
-          style={{ fontSize: "40px", color: "#576ca8" }}
-        ></ArrowCircleUpIcon>
+        {(() => {
+          if (inview1) {
+            return (
+              <ArrowCircleUpIcon
+                className="fadeup-overall"
+                style={{ fontSize: "40px", color: "#f7f7f7", zIndex: "100" }}
+              ></ArrowCircleUpIcon>
+            );
+          } else {
+            return (
+              <ArrowCircleUpIcon
+                style={{ fontSize: "40px", color: "#576ca8", zIndex: "100" }}
+              ></ArrowCircleUpIcon>
+            );
+          }
+        })()}
       </Scroll>
-
       <div style={{ paddingTop: "5%" }}>
         <p style={{ marginLeft: "5%" }} className="title">
           Career
@@ -45,7 +86,9 @@ export default function Notfound() {
             <li class="timeline-list-item">
               <div class="date">2019年05月</div>
               <div class="content">
-                <p style={{ margin: "0px" }}>大学でプログラミングを触る</p>
+                <p style={{ margin: "0px" }}>
+                  大学でプログラミングを初めて触る。
+                </p>
                 <Image
                   src={"https://junya-727.github.io/blog/b0581.png"}
                   width={200}
@@ -58,9 +101,7 @@ export default function Notfound() {
             <li class="timeline-list-item">
               <div class="date">2020年11月</div>
               <div class="content">
-                <p style={{ margin: "0px" }}>
-                  本格的にPythonでの開発を始める。
-                </p>
+                <p style={{ margin: "0px" }}>Pythonでの開発を始める。</p>
                 <Image
                   src={"https://junya-727.github.io/blog/g08100.png"}
                   width={200}
@@ -73,7 +114,7 @@ export default function Notfound() {
               <div class="date">2021年09月</div>
               <div class="content">
                 <p style={{ margin: "0px" }}>
-                  ビットコインを自動取引するbotを作成して運用する。
+                  機械学習を用いたビットコイン自動取引botを作成して運用する。
                 </p>
                 <Image
                   src={"https://junya-727.github.io/blog/y1206.png"}
@@ -101,7 +142,7 @@ export default function Notfound() {
               <div class="date">2022年03月</div>
               <div class="content">
                 <p style={{ margin: "0px" }}>
-                  楽天株式会社にて1ヶ月のエンジニアとして実務インターン。
+                  楽天株式会社にて1ヶ月のエンジニアとして実務インターン。楽天ペイの開発。
                 </p>
                 <Image
                   src={"https://junya-727.github.io/blog/n0004.png"}
@@ -207,38 +248,143 @@ export default function Notfound() {
             </Swiper>
           </div>
           <div id="bio">
-            <p style={{ margin: "5px" }}>
-              Pythonでは機械学習分野が得意です、少しならReact,Next.jsが書けます。
+            <p style={{ margin: "5px" }} className="font-theme">
+              Pythonでは機械学習が好きで、TensorFlowを使います。
             </p>
-            <p style={{ margin: "5px" }}>Kaggle入賞とAWS SAA習得を目標。</p>
+            <p style={{ margin: "5px" }} className="font-theme">
+              少しならReact,Next.jsが書けます。
+            </p>
+            <p style={{ margin: "5px" }} className="font-theme">
+              今年はKaggle入賞とAWS SAA習得を目標。
+            </p>
           </div>
-          {
-            //<div id="buttons"></div>
-          }
-          <div
+
+          <div id="buttons">
+            <Scroll to="bottom" smooth="true">
+              <button>Follow</button>
+            </Scroll>
+            <Scroll to="contact" smooth="true">
+              <button id="msg">Message</button>
+            </Scroll>
+          </div>
+        </div>
+      </div>
+      <div style={{ paddingTop: "5%" }} id="contact">
+        <p style={{ marginLeft: "5%" }} className="title">
+          Contact
+        </p>
+        <div>
+          <div className="Form">
+            <form onSubmit={registerUser} id="mail">
+              <div class="Form-Item">
+                <p class="Form-Item-Label">
+                  <span class="Form-Item-Label-Required">必須</span>お名前
+                </p>
+                <input
+                  type="text"
+                  class="Form-Item-Input"
+                  id="name"
+                  name="name"
+                  placeholder="例）山田太郎"
+                  required
+                ></input>
+              </div>
+              <div className="Form-Item">
+                <p class="Form-Item-Label">
+                  <span class="Form-Item-Label-Required">必須</span>
+                  メールアドレス
+                </p>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="name@example.com"
+                  class="Form-Item-Input"
+                  required
+                />
+              </div>
+              <div className="Form-Item">
+                <p class="Form-Item-Label isMsg">
+                  <span class="Form-Item-Label-Required">必須</span>
+                  お問い合わせ内容
+                </p>
+                <textarea
+                  id="message"
+                  name="message"
+                  className="Form-Item-Textarea"
+                  rows="3"
+                  required
+                ></textarea>
+              </div>
+              <div className="Form-Item">
+                <button type="submit" className="Form-Btn" ref={ref1}>
+                  <a>送信する</a>;
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div
+          style={{
+            position: "absolute",
+            paddingTop: "30%",
+            textAlign: "center",
+            width: "100%",
+            zindex: "100",
+          }}
+        >
+          {inview1 && (
+            <div className="fadeup-logo2">
+              <IconButton
+                variant="outlined"
+                href="https://www.instagram.com/zerodivisionerror_/"
+                style={{ paddingRight: "4%" }}
+              >
+                <InstagramIcon
+                  fontSize="large"
+                  style={{ color: "eb34d5", zIndex: "100" }}
+                ></InstagramIcon>
+              </IconButton>
+              <IconButton
+                variant="outlined"
+                href="https://github.com/JUNYA-727"
+                style={{ paddingRight: "4%" }}
+              >
+                <GitHubIcon
+                  fontSize="large"
+                  style={{ color: "black", zIndex: "100" }}
+                ></GitHubIcon>
+              </IconButton>
+
+              <IconButton
+                variant="outlined"
+                href="https://twitter.com/JUNY4_"
+                style={{ paddingRight: "4%" }}
+              >
+                <TwitterIcon
+                  fontSize="large"
+                  style={{ color: "0099FF", zIndex: "100" }}
+                ></TwitterIcon>
+              </IconButton>
+            </div>
+          )}
+          <p
             style={{
               width: "100%",
               textAlign: "center",
-              marginTop: "10%",
-              marginBottom: "10%",
+              color: "white",
+              zIndex: "100",
+              position: "absolute",
             }}
           >
-            <button style={{ textAlign: "center", margin: "auto" }}>
-              <a
-                href="https://junya-727.github.io/blog"
-                style={{ margin: "22%" }}
-              >
-                Follow
-              </a>
-            </button>
-          </div>
-          {
-            //<button id="msg">Message</button>
-          }
+            created by Junya Kuramochi
+          </p>
         </div>
-      </div>
 
-      <div className=""></div>
+        <div id="bottom" className="sample"></div>
+      </div>
     </div>
   );
 }
