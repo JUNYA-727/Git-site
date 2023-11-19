618e5fd7b33e5ed134e3553c94b9cc0bc8fdc8d4
JUNYA-727 <k.junya0727@icloud.com>
2023-11-20 01:27:08 +0900

    fix:css

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import '../styles/Home.module.css'
 import '../components/main/main.css'
 import { MainProvider } from '../components/context/ButtonContext'
 import '../components/seconds/test.css'
-
+import '/styles/news.css'
 function MyApp({ Component, pageProps }) {
   return (
     <MainProvider>
